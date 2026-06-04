3ea4d4da9c58b05f0fbf2b2c1324ee3e72d09b72
Kosaraju Tanvi <tk8164@srmist.edu.in>
2026-06-04 10:46:18 +0530

    Initial commit: Spring Boot structural setup

---
+++ src/main/java/com/security/fileencryptionsystem/FileEncryptionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.security.fileencryptionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileEncryptionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileEncryptionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/fileencryptionsystem/FileEncryptionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.fileencryptionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileEncryptionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
